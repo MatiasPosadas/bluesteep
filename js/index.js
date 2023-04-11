5d447f51bf1dd96f65396a3d5df8977c45ede4e7
Matias Posadas <93284124+MatiasPosadas@users.noreply.github.com>
2023-04-11 00:44:46 -0300

    nav + mediaqueries

--- js/index.js
+++ js/index.js
@@ -2,9 +2,18 @@ const ham = document.querySelector('.ham');
 const enlaces = document.querySelector('.enlaces__menu');
 const barras = document.querySelectorAll('.ham span');
 const logo = document.querySelector('.logo');
+const nav = document.querySelector('.nav');
+
 
 ham.addEventListener('click', ()=> {
     enlaces.classList.toggle('activado');
     barras.forEach(child => {child.classList.toggle('animado')});
     logo.classList.toggle('fuera');
 });
+
+/*Nav background scroll*/
+
+window.addEventListener("scroll", function(){
+    var header = document.querySelector("nav");
+    header.classList.toggle("abajo", window.scrollY>100);
+})
